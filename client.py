ee2075e1951c17da2e29f26d8a634777538bf05a
jonathanlex1 <jonathanlexi02@gmail.com>
2025-06-25 21:02:48 +0700

    streamlit app

---
+++ client.py
@@ -0,0 +1,28 @@
+import streamlit as st 
+from module import stuff_document_summarization
+import os 
+
+st.title('Summarize your Document')
+
+if 'count' not in st.session_state : 
+    st.session_state.count = 0
+
+uploaded_file = st.file_uploader('Upload document', type=['pdf'])
+
+if uploaded_file : 
+    if st.session_state['count'] >= 3 : 
+        st.warning("You've reached the summarization limit")
+    else : 
+        temp_data_path = os.path.join('data/', uploaded_file.name)
+        with open(temp_data_path, 'wb') as f : 
+            f.write(uploaded_file.read())
+
+        summarization = stuff_document_summarization(temp_data_path)
+        if summarization : 
+            st.write(summarization)
+            st.session_state['count'] += 1 
+            os.remove(temp_data_path)
+
+
+    
+
